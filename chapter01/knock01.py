f03bed06d2e92a62310c5604bfb1cda6fdbdb2f7
mlieynua <mlieynua-trp@outlook.jp>
2021-09-19 10:39:43 +0900

    fix 00 01

--- chapter01/knock01.py
+++ chapter01/knock01.py
@@ -2,7 +2,6 @@
 01. 「パタトクカシーー」
 「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ．
 '''
-
 char = "パタトクカシーー"
-linked_char = char[0]+char[2]+char[4]+char[6]
+linked_char = char[0] + char[2] + char[4] + char[6]
 print(linked_char)
