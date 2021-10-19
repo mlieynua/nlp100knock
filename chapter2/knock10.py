21b2739058fa24f1d1ab60cbeaaf28ab81ef4754
mlieynua <mlieynua-trp@outlook.jp>
2021-10-19 20:13:19 +0900

    add chapter2

--- chapter2/knock10.py
+++
@@ -1,7 +0,0 @@
-'''
-10. 行数のカウント
-行数をカウントせよ．確認にはwcコマンドを用いよ．
-'''
-with open("chapter2/data/popular-names.txt") as f:
-    raw_count = len(f.readlines())
-print(raw_count)
--- chapter2/knock11.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cat chapter2/data/popular-names.txt | tr "\t" " " > chapter2/output/output_sh/output11.txt 
